af191ec9db9aa85772fddae0d63cd41f3d864e0b
Alex K <damned182@mail.ru>
2019-02-12 23:02:36 +0300

    validate form inputs

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -3,16 +3,18 @@ import React, { Component } from 'react';
 class SurveyField extends Component {
   constructor(props) {
     super(props);
-    console.log(props)
   }
 
   render() {
-    const { input, label } = this.props;
-    
+    const { input, label, meta: { error, touched } } = this.props;
+
     return (
       <div>
         <label>{label}</label>
-        <input {...input} />
+        <input {...input} style={{ marginBottom: '5[x' }} />
+        <div className="red-text" style={{ marginBottom: '20px' }}>
+          {touched && error}
+        </div>
       </div>
     );
   }
